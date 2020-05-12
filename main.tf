457ba8e1610b9c9aaffcab2c7838441e894dc515
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2020-05-12 11:58:34 -0300

    provissionando ec2 com ubunto t2.micro na AWS

---
+++ main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-03e33c1cefd1d3d74"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
